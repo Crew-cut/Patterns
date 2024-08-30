261d103efe11ea05c1e9346ed7fa623031e1269e
Crewcut93 <crew-cut93@yandex.by>
2024-08-30 23:16:00 +0300

    Factory

--- Test/src/Factory_method/Car.java
+++ Test/src/Factory_method/Car.java
@@ -5,14 +5,5 @@ public abstract class Car {
     private BodyTypes bodyType;
     private String model;
     private long modelYear;
-
-
     public abstract void create(Car car);
-
-    @Override
-    public String toString (){
-        return  '\n' +"Тип кузова - " + bodyType + '\n' +
-                "Модель - " + model + '\n' +
-                "Модельный год:" + modelYear;
-    }
 }
