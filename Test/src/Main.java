0b5c5e31baacfa5a19a9b3135f60d7193f65a7c8
Crewcut93 <crew-cut93@yandex.by>
2024-09-02 20:40:12 +0300

    Adapter

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -9,14 +9,14 @@ import State.MyContext;
 public class Main {
     public static void main(String[] args) {
         //Factory
-//        CarManufacturer audi = new AudiManufacturer();
-//        audi.produce(BodyTypes.HATCHBACK);
-//        CarManufacturer vag = new VAGManufacturer();
-//        vag.produce(BodyTypes.HATCHBACK);
+        CarManufacturer audi = new AudiManufacturer();
+        audi.produce(BodyTypes.HATCHBACK);
+        CarManufacturer vag = new VAGManufacturer();
+        vag.produce(BodyTypes.HATCHBACK);
         //Adapter
-//        Plug psu = new PowerSupplyUnit(new Phone());
-//        psu.plug();
-//        psu.unplug();
+        Plug psu = new PowerSupplyUnit(new Phone());
+        psu.plug();
+        psu.unplug();
         //State
         State state = new State1();
         MyContext context = new MyContext(state);
